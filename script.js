97b52beae1a6ae91e217613a035a40a009b50979
Mel C <melchrstmn@gmail.com>
2021-04-06 23:02:08 -0400

    added helper function to displayPhotos function

--- script.js
+++ script.js
@@ -21,13 +21,17 @@ function displayPhotos() {
     photosArray.forEach((photo) => {
         // create anchor to link to api
         const item = document.createElement('a')
-        item.setAttribute('href', photo.links.html)
-        item.setAttribute('target', '_blank')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
         //create img for photo
         const img = document.createElement('img')
-        img.setAttribute('src', photo.urls.regular)
-        img.setAttribute('alt', photo.alt_description)
-        img.setAttribute('title', photo.alt_description)
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
         //put img inside anchor, then put both inside imageContainer
         item.appendChild(img)
         imageContainer.appendChild(item)
