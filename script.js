56d81da80432621a94e5203db0c1e87889392ab6
Mel C <melchrstmn@gmail.com>
2021-04-06 22:39:16 -0400

    added fetch for api

---
+++ script.js
@@ -0,0 +1,18 @@
+//API
+const count = 10
+const apiKey = 'mp4gN_gTGNPr9n8Lty4Tm9RXyug0lsC66Kbm1-X-9eo'
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// get photos from api
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+
+    }
+}
+
+// on load
+getPhotos()
