6a442ecd30579887c53088ca5d98b2ee57802572
Mel C <melchrstmn@gmail.com>
2021-04-06 22:51:58 -0400

    added displayPhotos function, building

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ const count = 10
 const apiKey = 'mp4gN_gTGNPr9n8Lty4Tm9RXyug0lsC66Kbm1-X-9eo'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// create elements for links & photos, add to dom
+function displayPhotos() {
+    //run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // create anchor to link to api
+        const item = document.createElement('a')
+        item.setAttribute('href', photo.links.html)
+        item.setAttribute('target', '_blank')
+    })
+}
+
 // get photos from api
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl)
         photosArray = await response.json()
-        console.log(photosArray)
+        displayPhotos()
     } catch (error) {
 
     }
